515c2c6329164b44bdde511c4f7bd8b00298c79d
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-02-14 17:01:57 -0500

    WOOHOO coin changer works

--- coin_changer.rb
+++ coin_changer.rb
@@ -3,9 +3,8 @@ def penny_function(my_cents)
 
 	if (my_cents - 1) > 0
 		pennies = my_cents / 1
-	end
-	amount = my_cents - pennies  	#First 4 functions show cents 
-end									#remaining after coins taken off
+	end	
+end									
 
 
 def nickel_function(my_cents)
@@ -14,7 +13,6 @@ def nickel_function(my_cents)
 	if (my_cents - 5) > 0
 		nickels = (my_cents / 5).floor
 	end
-	amount = my_cents - (5 * nickels)
 end
 
 
@@ -24,7 +22,6 @@ def dime_function(my_cents)
 	if (my_cents - 10) > 0
 		dimes = (my_cents / 10).floor
 	end
-	amount = my_cents - (10 * dimes)
 end
 
 
@@ -35,23 +32,21 @@ def quarter_function(my_cents)
 	if (my_cents - 25) > 0
 		quarters = (my_cents / 25).floor
 	end
-	amount = my_cents - (25 * quarters)
 end
 
 
 def final_coin_function(my_cents)
 
 	hash_coins = {}
-	first = 0
-
-	first = my_cents - (quarter_function(my_cents))
-
-	second = first - (10 * dime_function(first))
-
-	third = second - (5 * nickel_function(second))
-
-	fourth = third - penny_function(third)
-
+										#written out 44 cents
+	first = quarter_function(my_cents) 	#first = 1
+	remain = my_cents - (first * 25) 	#remain = 44 - 25 = 19 
+	second = dime_function(remain)		#second =  1
+	remain2 = remain - (second * 10)  	#remain2 = 19 - 10 = 9
+	third = nickel_function(remain2)	#third = 1
+	remain3 = remain2 - (third * 5)		#remain3 = 9 - 5 = 4
+	fourth = penny_function(remain3)	#fourth = 4
+	
 	hash_coins[:quarter] = first
 	hash_coins[:dime] = second
 	hash_coins[:nickel] = third
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -5,32 +5,32 @@ class IndividualCoinChanger < MiniTest::Test
 	def test_4_returns_4penny
 		my_cents = 4
 		results = penny_function(my_cents)
-		assert_equal(0, results)
+		assert_equal(4, results)
 	end
 
 	def test_17_returns_3nickel
 		my_cents = 17
 		results = nickel_function(my_cents)
-		assert_equal(2, results)
+		assert_equal(3, results)
 	end
 
 	def test_53_returns_5dime
 		my_cents = 53
 		results = dime_function(my_cents)
-		assert_equal(3, results)
+		assert_equal(5, results)
 	end
 
 	def test_74_returns_24remaining
 		my_cents = 74
 		results = quarter_function(my_cents)
-		assert_equal(24, results)
+		assert_equal(2, results)
 	end
 end
 
-# class FinalCoinChanger < MiniTest::Test 
-# 	def test_44_returns_correctly
-# 		my_cents = 44
-# 		results = final_coin_function(my_cents)
-# 		assert_equal({:quarter => 1, :dime => 1, :nickel => 1, :penny => 4 }, results)
-# 	end
-# end
+class FinalCoinChanger < MiniTest::Test 
+	def test_44_returns_correctly
+		my_cents = 44
+		results = final_coin_function(my_cents)
+		assert_equal({:quarter => 1, :dime => 1, :nickel => 1, :penny => 4 }, results)
+	end
+end
